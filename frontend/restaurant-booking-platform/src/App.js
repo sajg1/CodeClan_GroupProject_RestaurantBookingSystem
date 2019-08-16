5b890d213603a39223cd4071cb4bce0dbd0bace0
Steve Gossip <stephengossip@gmail.com>
2019-08-16 22:01:10 +0100

    restructured files to add in relevant css files.

--- frontend/restaurant-booking-platform/src/App.js
+++ frontend/restaurant-booking-platform/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Main from './containers/Main';
-
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Restaurant Booking System</h1>
+    <div className="app">
       <Main />
     </div>
   );
--- frontend/restaurant-booking-platform/src/containers/Main.js
+++ frontend/restaurant-booking-platform/src/containers/Main.js
@@ -1,11 +1,12 @@
-import BookingsForm from '../components/BookingsForm';
-import BookingsList from '../components/BookingsList';
-import GuestsList from '../components/GuestsList';
+import BookingsForm from '../components/Bookings/BookingsForm';
+import BookingsList from '../components/Bookings/BookingsList';
+import GuestsList from '../components/Guests/GuestsList';
 import Home from '../components/Home';
 import ErrorPage from '../components/ErrorPage';
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NavBar from '../components/NavBar';
+import NavBar from '../components/NavBar/NavBar.js';
+import './Main.css';
 
 class Main extends Component {
   constructor(props) {
@@ -17,6 +18,7 @@ class Main extends Component {
       <Router>
         <React.Fragment>
           <NavBar />
+          <h1>Restaurant Booking System</h1>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/bookings" component={BookingsList} />
