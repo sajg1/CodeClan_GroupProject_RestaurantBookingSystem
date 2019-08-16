f9da9568ff2bb1eb7a9cd0b5680e36e490c6eabf
Steve Gossip <stephengossip@gmail.com>
2019-08-16 14:35:30 +0100

    added a NavBar and added router tags to main.js

---
+++ frontend/restaurant-booking-platform/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => (
+  <ul>
+    <li>This is a NavBar
+    </li>
+  </ul>
+)
+
+export default NavBar;
---
+++ frontend/restaurant-booking-platform/src/containers/Main.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <Router>
+        <React.Fragment>
+          <NavBar />
+        </React.Fragment>
+      </Router>
+
+    )
+  }
+}
+
+export default Main;
