d36eb59996348f4dbf919ae0bf4649680bdc81ef
Steve Gossip <stephengossip@gmail.com>
2019-08-16 20:47:15 +0100

    added error page that returns a 404 when the wrong extension is entered into the address bar.

---
+++ frontend/restaurant-booking-platform/src/components/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => (
+  <div>
+    <p>--------------------------</p>
+    <h1>404 - Page Not Found</h1>
+    <p>--------------------------</p>
+  </div>
+
+)
+
+export default ErrorPage;
--- frontend/restaurant-booking-platform/src/containers/Main.js
+++ frontend/restaurant-booking-platform/src/containers/Main.js
@@ -2,6 +2,7 @@ import BookingsForm from '../components/BookingsForm';
 import BookingsList from '../components/BookingsList';
 import GuestsList from '../components/GuestsList';
 import Home from '../components/Home';
+import ErrorPage from '../components/ErrorPage';
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from '../components/NavBar';
@@ -20,6 +21,7 @@ class Main extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/bookings" component={BookingsList} />
             <Route path="/guests" component={GuestsList} />
+            <Route component={ErrorPage} />
           </Switch>
         </React.Fragment>
       </Router>
