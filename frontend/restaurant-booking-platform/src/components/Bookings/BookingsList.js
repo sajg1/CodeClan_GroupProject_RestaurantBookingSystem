6db19cbcc866f8dada8a52d23024d4f3052c565a
George Gordon <georgegordon101@gmail.com>
2019-08-17 12:25:26 +0100

    Completed fetchRequest and proped bookings down to booking

--- frontend/restaurant-booking-platform/src/components/Bookings/BookingsList.js
+++ frontend/restaurant-booking-platform/src/components/Bookings/BookingsList.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const BookingsList = () => {
+const BookingsList = (props) => {
+
+  const bookingItems = props.bookings.map((booking, index) => {
+    return <li key="index">{booking.numberPeople}, {booking.dateTime}</li>
+
+  });
+
   return(
     <div className="booking-list">
       <h2>Restaurant Bookings</h2>
       <p>List of Bookings</p>
+      {bookingItems}
     </div>
   )
 
