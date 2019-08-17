81fba01a0ee8b513f30cba35c3cba5a2e37e0c4e
George Gordon <georgegordon101@gmail.com>
2019-08-17 13:12:23 +0100

    fixed fetch request

--- frontend/restaurant-booking-platform/src/containers/Main.js
+++ frontend/restaurant-booking-platform/src/containers/Main.js
@@ -7,10 +7,26 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from '../components/NavBar/NavBar.js';
 import './Main.css';
+import Request from '../helpers/request';
 
 class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      bookings: []
+    }
+  }
+
+  componentDidMount() {
+    const request = new Request()
+    const bookingsPromise = request.get('/api/bookings');
+    console.log(bookingsPromise);
+    const promises = [bookingsPromise];
+    Promise.all(promises).then((data) => {
+      this.setState({
+        bookings: data[0]._embedded.bookings
+      })
+    })
   }
 
   render() {
@@ -21,8 +37,13 @@ class Main extends Component {
           <h1>Restaurant Booking System</h1>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/bookings" component={BookingsList} />
             <Route path="/guests" component={GuestsList} />
+            <Route
+              exact path="/bookings"
+              render={(props) => {
+                return <BookingsList bookings={this.state.bookings} />
+              }}
+            />
             <Route component={ErrorPage} />
           </Switch>
         </React.Fragment>
