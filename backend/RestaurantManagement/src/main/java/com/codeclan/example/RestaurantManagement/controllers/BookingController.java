15cfaa00aa609045728dc85f3217284064696a36
Jeremy Wood <jeremy.wood.nz@gmail.com>
2019-08-17 14:37:47 +0100

    updated routes to include /api

--- backend/RestaurantManagement/src/main/java/com/codeclan/example/RestaurantManagement/controllers/BookingController.java
+++ backend/RestaurantManagement/src/main/java/com/codeclan/example/RestaurantManagement/controllers/BookingController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bookings")
+@RequestMapping("/api/bookings")
 public class BookingController {
 
     @Autowired
