34fbed2c1d1de4e9c0d113de7a34fb10b1fa8635
Steve Gossip <stephengossip@gmail.com>
2019-08-17 11:10:21 +0100

    installed enzyme for shallow testing on frontend. Created a setupTests.js and a Main.test.js file

---
+++ frontend/restaurant-booking-platform/src/containers/Main.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './Main';
+import NavBar from './NavBar/NavBar';
+import BookingsList from '../components/Bookings/BookingsList';
+import GuestsList from '../components/Guests/GuestsList';
+import Home from '../components/Home';
+import ErrorPage from '../components/ErrorPage';
+import {shallow} from 'enzyme';
+
+describe("Main", () => {
+
+});
