1aa8199d27dbe67fbec5cf482423b661115ea7dd
Jeremy Wood <jeremy.wood.nz@gmail.com>
2019-08-19 13:25:29 +0100

    embedded customer and restaurantTable

--- backend/RestaurantManagement/src/main/java/com/codeclan/example/RestaurantManagement/models/projections/EmbedBookingAndRestaurantTable.java
+++ backend/RestaurantManagement/src/main/java/com/codeclan/example/RestaurantManagement/models/projections/EmbedBookingAndRestaurantTable.java
@@ -1,4 +1,31 @@
 package com.codeclan.example.RestaurantManagement.models.projections;
 
-public class EmbedBookingAndRestaurantTable {
+import com.codeclan.example.RestaurantManagement.models.Booking;
+import com.codeclan.example.RestaurantManagement.models.Customer;
+import com.codeclan.example.RestaurantManagement.models.RestaurantTable;
+import com.codeclan.example.RestaurantManagement.repositories.RestaurantTableRepository.RestaurantTableRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDateTime;
+
+@Projection(name = "embedBookingAndRestaurantTable", types = Booking.class)
+public interface EmbedBookingAndRestaurantTable {
+    Long getId();
+
+    int getNumberPeople();
+
+    LocalDateTime getDateTime();
+
+    String getAdditionalInfo();
+
+    Customer getCustomer();
+
+    RestaurantTable getRestaurantTable();
+
+//    @Value("#{target.getCustomer().getId()}")
+//    Long getCustomerID();
+//
+//    @Value("#{target.getRestaurantTable().getId()}")
+//    Long getRestaurantTable();
 }
--- backend/RestaurantManagement/src/main/java/com/codeclan/example/RestaurantManagement/repositories/BookingRepository/BookingRepository.java
+++ backend/RestaurantManagement/src/main/java/com/codeclan/example/RestaurantManagement/repositories/BookingRepository/BookingRepository.java
@@ -2,13 +2,15 @@ package com.codeclan.example.RestaurantManagement.repositories.BookingRepository
 
 import com.codeclan.example.RestaurantManagement.models.Booking;
 import com.codeclan.example.RestaurantManagement.models.RestaurantTable;
+import com.codeclan.example.RestaurantManagement.models.projections.EmbedBookingAndRestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
+@RepositoryRestResource(excerptProjection = EmbedBookingAndRestaurantTable.class)
 public interface BookingRepository extends JpaRepository<Booking,Long> {
     List<Booking> findAllBookingsByDateTimeBetween(LocalDateTime startDate,LocalDateTime endDate);
 }
