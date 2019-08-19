c8dccb4c191a78105caa1988aa846cf6250ff83b
Jeremy Wood <jeremy.wood.nz@gmail.com>
2019-08-19 09:59:59 +0100

    added ids to responses for all models

--- backend/RestaurantManagement/src/main/java/com/codeclan/example/RestaurantManagement/configurations/SpringGlobalConfig.java
+++ backend/RestaurantManagement/src/main/java/com/codeclan/example/RestaurantManagement/configurations/SpringGlobalConfig.java
@@ -1,8 +1,12 @@
 package com.codeclan.example.RestaurantManagement.configurations;
 
+import com.codeclan.example.RestaurantManagement.models.Booking;
+import com.codeclan.example.RestaurantManagement.models.Customer;
+import com.codeclan.example.RestaurantManagement.models.RestaurantTable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +20,7 @@ public class SpringGlobalConfig implements RepositoryRestConfigurer, WebMvcConfi
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Booking.class, Customer.class,RestaurantTable.class);
         config.getCorsRegistry()
                 .addMapping(CORS_BASE_PATTERN)
                 .allowedOrigins(ALLOWED_ORIGINS)
@@ -30,4 +35,5 @@ public class SpringGlobalConfig implements RepositoryRestConfigurer, WebMvcConfi
                 .allowedHeaders(ALLOWED_HEADERS)
                 .allowedMethods(ALLOWED_METHODS);
     }
+
 }
