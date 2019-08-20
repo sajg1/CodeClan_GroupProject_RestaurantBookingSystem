cee329ab65702f4262ddaaad63f64c54d8c03424
Simon <shwaldemar@gmail.com>
2019-08-20 13:03:28 +0100

    amended time and date display on bookings list to show day and remove seconds

--- frontend/restaurant-booking-platform/src/components/Bookings/BookingsList.js
+++ frontend/restaurant-booking-platform/src/components/Bookings/BookingsList.js
@@ -3,21 +3,34 @@ import Booking from './Booking';
 
 class BookingsList extends Component {
   render() {
+
     const bookingNodes = this.props.bookings.map(booking => {
+
+      let date = new Date(booking.dateTime);
+
+      var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: ('numeric', '2-digit'), minute: ('numeric', '2-digit') };
+   
       return (
         <Booking value={booking.value} key={booking.id}>
-        When: {booking.dateTime},
-        Who: {booking.customer.name},
-        How Many: {booking.numberPeople},
-        What Table: {booking.restaurantTable.num}
+        {date.toLocaleString('en-GB', options)},
+        {booking.customer.name},
+        {booking.numberPeople},
+        {booking.restaurantTable.num}
+
         </Booking>
       );
+
     });
 
     return (
       <div className="booking-list">
+
         <h2>Restaurant Bookings</h2>
-          {bookingNodes}
+
+            <div>
+              {bookingNodes}
+            </div>
+
       </div>
     )
 }
